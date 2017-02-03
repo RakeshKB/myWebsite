7eefec0fb588008e68c7a5c522c631f4e902bc1e
RakeshKB <rakesh@mediboxs-MacBook-Air.local>
2017-02-03 18:02:54 +0530

    initializibg homeController for displaying name on the web page

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,3 +1,8 @@
 /**
  * Created by rakesh on 03/02/17.
  */
+
+app.controller('homeController', ['$scope',function($scope){
+    console.log("homeController called");
+    $scope.title = 'Rakesh KB';
+}]);
